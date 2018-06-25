b5bc08f0fd6088b2c58fe8646114d2f413114be0
Ron Lynch <lynch.ronald1@gmail.com>
2018-06-25 02:14:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // write your code below!
 
-function happyHolidays
+function happyHolidays() {
+  return ("Happy holidays!")
+}
+
+function happyHolidaysTo() {
+  return (`Happy holidays, ${name}!`)
+}
+
+function happyHolidayTo
